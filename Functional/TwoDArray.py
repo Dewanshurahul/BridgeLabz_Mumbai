f1dd4e425f3d2789530a8fab0fe8a6c6273bddc1
Dewanshu Rahul <dewanshurahul95@gmail.com>
2020-02-14 19:07:37 +0530

    [Dewanshu Rahul]Refactor. Adding Comment and writing WindChill Program.

--- Functional/TwoDArray.py
+++ Functional/TwoDArray.py
@@ -1,2 +1,15 @@
-row = int(input("Enter ROW: "))
-cloumn = int(input("Enter COLUMN: "))
+#Creating a 2-D dynamic Array and Printing it's Elememt
+rows = int(input("Enter ROW: "))
+columns = int(input("Enter COLUMN: "))
+#An Array of row * Columns is being created
+myArray = [[ 0 for i in range(columns)] for j in range(rows)]
+print(myArray)
+for row in range(rows):
+    for column in range(columns):
+        #Assigning the User defined values to the Array
+        myArray[row][column] = input("Enter")
+print(myArray)
+#Printing the Array Element-Wise
+for row in range(rows):
+    for column in range(columns):
+        print(myArray[row][column])
