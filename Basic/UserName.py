74872b8d50d8de1ede402464fa9d36b3188df066
Dewanshu Rahul <dewanshurahul95@gmail.com>
2020-02-15 10:18:47 +0530

    [Dewanshu Rahul]Refactor. Modifing the code with RegEx Code and adding comment to it.

--- Basic/UserName.py
+++ Basic/UserName.py
@@ -1,4 +1,7 @@
+# Replacing a part of given String with desired content using regular expression.
+import re
 name = input("Enter your Name: ")
 givenString = "Hello <<UserName>>, How are you?"
-requiredString = givenString.replace("<<UserName>>", name)
+requiredString = re.sub("<<UserName>>", name, givenString)
+#requiredString = givenString.replace("<<UserName>>", name)
 print(requiredString)
