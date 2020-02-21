81de82b21b66896581eb475b8d384216bce32515
Dewanshu Rahul <dewanshurahul95@gmail.com>
2020-02-21 19:15:48 +0530

    [Dewanshu Rahul]Added: Programs Containing Operations on Tuple.

--- MLProblem/Data Structure/Tuple/TuplePrograms.py
+++ MLProblem/Data Structure/Tuple/TuplePrograms.py
@@ -1,8 +1,67 @@
 class Tuple:
 
+    # To print Every Element of passed Tuple
+    # def show(self,tupleOfElements):
+    #     for element in tupleOfElements:
+    #         print(element,end=" ")
+    #     print()
+
+    # To create and return a empty Tuple
     def createTuple(self):
         return ()
 
+    # Accepts the data to create and return a tuple
+    def create(self, *args):
+        return args
+
+    # Copy and return a new Tuple from the passed Tuple
+    def clone(self, givenTuple):
+        return tuple(element for element in givenTuple)
+
+    """
+    Unpack a Tuple in different Variale
+    For that the Number of varable must be
+    equal to the Number of element in the Tuple
+    """
+    newTuple = ("Dewanshu", "Rahul", 23)
+    firstName, lastName, age = newTuple
+    print(firstName, lastName, age)
+
+    # Return repeated item(s) inside a tuple
+    def repeatedItem(self, givenTuple):
+        tempList = []
+        for index in range(len(givenTuple)):
+            for ind in range(index + 1, len(givenTuple)):
+                if givenTuple[index] == givenTuple[ind]:
+                    tempList.append(givenTuple[index])
+        return tuple(tempList)
+
+    # Return True if passed element is present in Tuple or
+    # Return False if passes element is not present in Tuple
+    def exists(self, element, givenTuple):
+        return element in givenTuple
+
+    # Takes a Sequence as an input and return back a Tuple
+    def convertToTuple(self, listOfItems):
+        return tuple(element for element in listOfItems)
+
+    # Remove a particular element from the Tuple
+    def remove(self, ele, tupleOfElement):
+        tempList = []
+        for element in tupleOfElement:
+            if element != ele:
+                tempList.append(element)
+        return tuple(tempList)
+
+    # Slice a tuple based on passed index
+    def slice(self, firstIndex, lastIndex, givenTuple):
+        return tuple(list(givenTuple)[firstIndex:lastIndex])
+
+    # Reverse the passed Tuple
+    def reverse(self, givenTuple):
+        return tuple(reversed(givenTuple))
+
 
 t = Tuple()
-print(t.createTuple())
+tup = ("Dewanshu", "Rahul", 23, "Dewanshu", 23)
+print(t.repeatedItem(tup))
