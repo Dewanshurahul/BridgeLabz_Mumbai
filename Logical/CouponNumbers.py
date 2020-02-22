a8008c1c4d36a53c4b36742953afec58d8f6d664
Dewanshu Rahul <dewanshurahul95@gmail.com>
2020-02-22 10:21:22 +0530

    [Dewanshu Rahul]Refactor: Modifing the Variable Containing List Of Randoms

--- Logical/CouponNumbers.py
+++ Logical/CouponNumbers.py
@@ -1,14 +1,14 @@
 from random import random
 
 size = int(input("Enter the Number of Unique Copons You Want: "))
-lst = []
+listOfRandom = []
 count = 0
 while True:
     number = (random()*100) // 10
     count += 1
-    if number not in lst:
-        lst.append(number)
-    if len(lst) == size:
+    if number not in listOfRandom:
+        listOfRandom.append(number)
+    if len(listOfRandom) == size:
         break
 print(count)
-print(lst)
+print(listOfRandom)
