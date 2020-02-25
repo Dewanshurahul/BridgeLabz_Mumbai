681bb8e47ca5e99cfaddb6c6d5573af6986529d8
Dewanshu Rahul <dewanshurahul95@gmail.com>
2020-02-25 12:18:33 +0530

    Rafactor: Instance to Static and Fixing function symmetricDifference.

--- MLProblem/Data Structure/Set/Sets.py
+++ MLProblem/Data Structure/Set/Sets.py
@@ -1,35 +1,35 @@
 class Sets:
 
     #Return the Empty Set
-    def createSet(self):
+    def createSet():
         return set({})
 
     # Print every element of the passed Set
-    def show(self, setOfElements):
+    def show(setOfElements):
         for values in setOfElements:
             print(values, end=" ")
         print()
 
     # Add the passed element into the Given Set
-    def addElement(self, element, setOfElement):
+    def addElement(element, setOfElement):
         setOfElement.add(element)
         return setOfElement
 
     # Take element(s) and remove from the passed Set if Present
-    def removeItems(self, setOfElement, *element):
+    def removeItems(setOfElement, *element):
         for index in range(len(element)):
             if element[index] in setOfElement:
                 setOfElement.remove(element[index])
         return setOfElement
 
     # Take one element and remove it from the Set if Present
-    def removeMember(self, element, setOfElement):
+    def removeMember(element, setOfElement):
         if element in setOfElement:
             setOfElement.remove(element)
         return setOfElement
 
     # Return the element which are common in both the Set
-    def intersectionOfSets(self, firstSet, secondSet):
+    def intersectionOfSets(firstSet, secondSet):
         intersectionSet = set({})
         for firstvalues in firstSet:
             for secondvalues in secondSet:
@@ -38,7 +38,7 @@ class Sets:
         return intersectionSet
 
     # Return the elemets of both the Sets **UNION(Mathematical)**
-    def unionOfSets(self, firstSet, secondSet):
+    def unionOfSets(firstSet, secondSet):
         unionSet = set({})
         for values in firstSet:
             unionSet.add(values)
@@ -47,7 +47,7 @@ class Sets:
         return unionSet
 
     # Return a new set with elements in the set that are not in the others
-    def setDifference(self,firstSet,secondSet):
+    def setDifference(firstSet,secondSet):
         differenceSet = set({})
         for firstValues in secondSet:
             for secondValues in firstSet:
@@ -58,15 +58,21 @@ class Sets:
         return differenceSet
 
 
-    # def symmetricDifference(self,firstSet,secondSet):
-    #     differenceSet = set({})
-    #     for firstValues in secondSet:
-    #         for secondValues in firstSet:
-    #             if firstValues == secondValues:
-    #                 break
-    #         else:
-    #             differenceSet.add(firstValues)
-    #     return differenceSet
+    def symmetricDifference(firstSet,secondSet):
+        differenceSet = set({})
+        for firstValue in secondSet:
+            for secondValue in firstSet:
+                if firstValue == secondValue:
+                    break
+            else:
+                differenceSet.add(firstValue)
+        for firstValue in firstSet:
+            for secondValue in secondSet:
+                if firstValue == secondValue:
+                    break
+            else:
+                differenceSet.add(firstValue)
+        return differenceSet
 
 
     # def frozenSet(self):
@@ -74,13 +80,13 @@ class Sets:
 
 
     # Empty the passed Set
-    def clear(self,setOfElement):
+    def clear(setOfElement):
         while len(setOfElement) != 0:
             setOfElement.pop()
         return setOfElement
 
     # Return the minimum element of the Array
-    def min(self, setOfElement):
+    def min(setOfElement):
         if len(setOfElement) != 0:
             minimum = setOfElement.pop()
             setOfElement.add(minimum)
@@ -92,7 +98,7 @@ class Sets:
         return minimum
 
     # Return the Maximum element of the Array
-    def max(self, setOfElement):
+    def max(setOfElement):
         if len(setOfElement) != 0:
             maximum = setOfElement.pop()
             setOfElement.add(maximum)
@@ -109,4 +115,4 @@ class Sets:
 s = Sets()
 sets = {7, 2, 5, 4, 6, 45}
 secondSet = { 50,42,65,49,12,21,6,7,5 }
-print(s.max(sets))
+print(Sets.symmetricDifference(sets,secondSet))
