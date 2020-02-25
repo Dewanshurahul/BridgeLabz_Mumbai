4bbea66d6085a5eef5a43e7cf486171feb986841
Dewanshu Rahul <dewanshurahul95@gmail.com>
2020-02-25 11:16:06 +0530

    Refactor: Converting all the Instance Functions into Static Functions.

--- MLProblem/Data Structure/Dictionary/Dictionary.py
+++ MLProblem/Data Structure/Dictionary/Dictionary.py
@@ -4,7 +4,7 @@ class Dictionary:
     # Return the sorted Dictionary based on values in Ascending Order
     # But Not Completely Working (Only Values are Sorted Not the Keys)
 
-    # def sortAsc(self, dictionary):
+    # def sortAsc(dictionary):
     #     for index in list(dictionary):
     #         for ind in list(dictionary):
     #             if dictionary.get(index) < dictionary.get(ind):
@@ -14,16 +14,16 @@ class Dictionary:
     #     return dictionary
 
     # Return the sorted Dictionary based on values in Ascending Order
-    def sortAsc(self,givenDictionary):
+    def sortAsc(givenDictionary):
         return sorted(givenDictionary.items(),key=operator.itemgetter(1))
 
     # Add a key value pair to the existing dictionary
-    def add(self,key,value,givenDictionary):
+    def add(key,value,givenDictionary):
         givenDictionary[key] = value
         return givenDictionary
 
     # Concatinate all the passed dictionaries
-    def concatinate(self, *args):
+    def concatinate(*args):
         requiredDictionary = {}
         for index in range(len(args)):
             for element in args[index]:
@@ -31,7 +31,7 @@ class Dictionary:
         return requiredDictionary
 
     # Print each Key : Value Pair
-    def show(self,givenDictionary):
+    def show(givenDictionary):
         for element in givenDictionary:
             print(str(element),":",str(givenDictionary.get(element)))
 
@@ -41,14 +41,14 @@ class Dictionary:
     and return the Dictionary
     e.g {1:1, 2:4, 3:9, 4:16 ...........}
     """
-    def generatePowerDictionary(self,upto):
+    def generatePowerDictionary(upto):
         requiredDictionary = {}
         for value in range(1,upto+1):
             requiredDictionary[value] = value*value
         return requiredDictionary
 
     # Remove the Element based on the passed key
-    def remove(self,key,givenDictionary):
+    def remove(key,givenDictionary):
         requiredDictionary = {}
         for element in givenDictionary:
             if element == key:
@@ -58,14 +58,14 @@ class Dictionary:
         return requiredDictionary
 
     # Return the Unique Values from the the Dictionary in the form of List
-    def uniqueValues(self,givenDictionary):
+    def uniqueValues(givenDictionary):
         tempList = []
         for keys in givenDictionary:
             tempList.append(givenDictionary.get(keys))
         return set(tempList)
 
     # Return a dictionary with the character as keys and there count as Values from the passed String
-    def occurenceOfCharacter(self,givenString):
+    def occurenceOfCharacter(givenString):
         requiredDictionary = {}
         for index in range(len(givenString)):
             count = 0
@@ -77,12 +77,12 @@ class Dictionary:
         return requiredDictionary
 
     # Print the Passed Dictionary as Table
-    def printTable(self,givenDictionary):
+    def printTable(givenDictionary):
         for element in givenDictionary:
             print(element +" "+str(givenDictionary.get(element)))
 
     # Return the True Count value for a Key in Dictionary
-    def countTrue(self,givenDictionary):
+    def countTrue(givenDictionary):
         count = 0
         for element in givenDictionary:
             if element == "success" and givenDictionary.get(element) =="True":
@@ -90,7 +90,7 @@ class Dictionary:
         return count
 
     # Take a list as an Argument and Return the Nested Dictionay of Keys
-    # def nestedDictionary(self,listOfKeys):
+    # def nestedDictionary(listOfKeys):
     #     current = newDictionary = {}
     #     for keys in listOfKeys:
     #         newDictionary[keys] = {}
@@ -98,7 +98,7 @@ class Dictionary:
     #     return current
 
     # Return True if Duplicate Keys are Present else Return False
-    def multipleKeys(self,givenDictionary):
+    def multipleKeys(givenDictionary):
         for element in givenDictionary:
             flag = 0
             for ele in givenDictionary:
@@ -109,7 +109,7 @@ class Dictionary:
         return True
 
     # Count the Number of list Present in the passed Dictionary as Value
-    def countList(self,givenDictionary):
+    def countList(givenDictionary):
         count = 0
         for element in givenDictionary:
             if type(givenDictionary.get(element)) == list:
@@ -119,9 +119,8 @@ class Dictionary:
 
 # Code For Testing the Above Written Codes(Functions)
 
-d = Dictionary()
 string = "DewDewanahe"
 detail = {"name": 35, "age": 23}
 lst = ["name","age","place","mind"]
-#print(d.occurenceOfCharacter(string))
-d.printTable(detail)
+print(Dictionary.occurenceOfCharacter(string))
+Dictionary.printTable(detail)
