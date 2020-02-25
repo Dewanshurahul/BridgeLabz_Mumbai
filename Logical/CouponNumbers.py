7c92049fbb9a491d18b117e1dd1442feecbbcd8f
Dewanshu Rahul <dewanshurahul95@gmail.com>
2020-02-25 10:04:30 +0530

    Rafactor: Adding Functionality to control Random Number Generation

--- Logical/CouponNumbers.py
+++ Logical/CouponNumbers.py
@@ -3,12 +3,21 @@ from random import random
 size = int(input("Enter the Number of Unique Copons You Want: "))
 listOfRandom = []
 count = 0
+# Balance the Range of Random number is being Generated
+def pow():
+    rangeOfRandom = 10
+    temp = size
+    while temp != 0:
+        rangeOfRandom *= 10
+        temp //= 10
+    return rangeOfRandom
+
 while True:
-    number = (random()*100) // 10
+    number = (random()*pow()) // 10
     count += 1
     if number not in listOfRandom:
         listOfRandom.append(number)
     if len(listOfRandom) == size:
         break
-print(count)
+print("Total Number of Random Number Generated: ",count)
 print(listOfRandom)
