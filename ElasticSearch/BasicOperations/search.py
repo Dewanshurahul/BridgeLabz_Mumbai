ce8f9702a7093e3a9dab3cc97646523fadd9f0c0
Dewanshu Rahul <dewanshurahul95@gmail.com>
2020-06-15 10:02:40 +0530

    Refactor: Modifing searching Logic.

--- ElasticSearch/BasicOperations/search.py
+++ ElasticSearch/BasicOperations/search.py
@@ -2,5 +2,15 @@ from elasticsearch import Elasticsearch
 try:
     es = Elasticsearch([{"host":"localhost","port":9200}])
     print(es)
+    res = es.search(index="studentinfo", body={'query': {
+        'bool': {
+            'must': [{
+                'match': {
+                    'first_name': 'Dewanshu'
+                }
+            }]
+        }
+    }})
+    print(res)
 except:
     print("Connection not Established")
